edb8866985e57e6346e44e0a38b321c41bee2e59
CharlesAntoineParent <charles-antoine.parent@outlook.com>
2021-04-30 13:51:52 -0400

    ajout de modele

--- lib/lib_models/irma.py
+++ lib/lib_models/irma.py
@@ -12,38 +12,23 @@ import torchvision.models as models
 class Encoder(nn.Module):
     def __init__(self,capacity):
         super(Encoder, self).__init__()
-        c = capacity
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=c, kernel_size=4, stride=2, padding=1) # out: c x 14 x 14
-        self.conv2 = nn.Conv2d(in_channels=c, out_channels=c*2, kernel_size=4, stride=2, padding=1) # out: c x 7 x 7
-        self.conv3 = nn.Conv2d(in_channels=c*2, out_channels=c, kernel_size=1) # out: c x 7 x 7
-        self.fc = nn.Linear(in_features=c*7*7, out_features=latent_dims)
+        self.fc1 = nn.Linear(784, 900, bias = False)
         
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(x.size(0), -1) # flatten batch of multi-channel feature maps to a batch of feature vectors
-        x = self.fc(x)
-        
-        return x
+        h1 = self.relu(self.fc1(x.view(-1, 784)))
+        return h1
+
 
 class Decoder(nn.Module):
     def __init__(self,capacity):
         super(Decoder, self).__init__()
-        self.c = capacity
-        self.fc = nn.Linear(in_features=latent_dims, out_features=c*7*7)
-        self.conv3 = nn.ConvTranspose2d(in_channels=self.c, out_channels=c*2, kernel_size=1)
-        self.conv2 = nn.ConvTranspose2d(in_channels=self.c*2, out_channels=c, kernel_size=4, stride=2, padding=1)
-        self.conv1 = nn.ConvTranspose2d(in_channels=self.c, out_channels=1, kernel_size=4, stride=2, padding=1)
+        self.fc2 = nn.Linear(900, 784, bias = False)
         
             
     def forward(self, x):
-        x = self.fc(x)
-        x = x.view(x.size(0), self.c, 7, 7) # unflatten batch of feature vectors to a batch of multi-channel feature maps
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv2(x))
-        x = torch.tanh(self.conv1(x)) # last layer before output is tanh, since the images are normalized and 0-centered
-        return x
+        h2 = self.sigmoid(self.fc2(z))
+        return h2
+
 
 class LNN(nn.Module):
     def __init__(self, num_of_linear_layers, type):
@@ -51,22 +36,22 @@ class LNN(nn.Module):
         super(LNN, self).__init__()
 
         if type == "linear":
-            self.linear_layer = nn.ModuleList([nn.Linear(in_features=latent_dims, out_features=latent_dims) for _ in range(num_of_linear_layers)])
+            self.linear_layer = nn.ModuleList([nn.Linear(in_features=900, out_features=900) for _ in range(num_of_linear_layers)])
             self.forward = self.forward_linear
 
         if type == "weight_share":
-            self.linear_layer = nn.Linear(in_features=latent_dims, out_features=latent_dims)
+            self.linear_layer = nn.Linear(in_features=900, out_features=900)
             self.forward = self.forward_weight_share
 
         if type == "fixed":
-            self.linear_layer = nn.ModuleList([nn.Linear(in_features=latent_dims, out_features=latent_dims) for _ in range(num_of_linear_layers)])
+            self.linear_layer = nn.ModuleList([nn.Linear(in_features=900, out_features=900) for _ in range(num_of_linear_layers)])
             for layer in self.linear_layer:
                 for param in layer.parameters():
                     param.requires_grad = False
             self.forward = self.forward_fixed
 
         if type == "nonlinear":
-            self.linear_layer = nn.ModuleList([nn.Linear(in_features=latent_dims, out_features=latent_dims) for _ in range(num_of_linear_layers)])
+            self.linear_layer = nn.ModuleList([nn.Linear(in_features=900, out_features=900) for _ in range(num_of_linear_layers)])
             self.forward = self.forward_nonlinear
         
     def forward_linear(self, x):
@@ -99,7 +84,20 @@ class Irma(nn.Module):
         self.decoder = Decoder(capacity)
     
     def forward(self, x):
-        latent = self.encoder(x)
-        min_rank_latent = self.lnn(latent)
-        x_recon = self.decoder(min_rank_latent)
-        return x_recon
+        h1 = self.encoder(x)
+        min_rank_latent = self.lnn(h1)
+        h2 = self.decoder(min_rank_latent)
+        return h1,h2
+
+
+
+class AE(nn.Module):
+    def __init__(self,capacity, *args, **kwargs):
+        super(Irma, self).__init__()
+        self.encoder = Encoder(capacity)
+        self.decoder = Decoder(capacity)
+    
+    def forward(self, x):
+        h1 = self.encoder(x)
+        h2 = self.decoder(h1)
+        return h1,h2        
