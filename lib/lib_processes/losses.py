d2dd178960b24f5b06f8530e879d21a7eb0bc957
CharlesAntoineParent <charles-antoine.parent@outlook.com>
2021-05-01 18:14:23 -0400

    correction loss

--- lib/lib_processes/losses.py
+++ lib/lib_processes/losses.py
@@ -14,25 +14,26 @@ def CrossEntropy_criterion(input_batch, target_batch):
 
 mse_loss = nn.BCELoss(size_average = False)
 
-def loss_function(W, x, recons_x, h, lam):
+def loss_function(W, target_batch, input_batch, h):
     """Compute the Contractive AutoEncoder Loss
     Evalutes the CAE loss, which is composed as the summation of a Mean
     Squared Error and the weighted l2-norm of the Jacobian of the hidden
     units with respect to the inputs.
     See reference below for an in-depth discussion:
-      #1: http://wiseodd.github.io/techblog/2016/12/05/contractive-autoencoder
+        #1: http://wiseodd.github.io/techblog/2016/12/05/contractive-autoencoder
     Args:
         `W` (FloatTensor): (N_hidden x N), where N_hidden and N are the
-          dimensions of the hidden units and input respectively.
+            dimensions of the hidden units and input respectively.
         `x` (Variable): the input to the network, with dims (N_batch x N)
         recons_x (Variable): the reconstruction of the input, with dims
-          N_batch x N.
+            N_batch x N.
         `h` (Variable): the hidden units of the network, with dims
-          batch_size x N_hidden
+            batch_size x N_hidden
         `lam` (float): the weight given to the jacobian regulariser term
     Returns:
         Variable: the (scalar) CAE loss
     """
+    lam = 1e-4
     mse = mse_loss(recons_x, x)
     # Since: W is shape of N_hidden x N. So, we do not need to transpose it as
     # opposed to #1
@@ -44,3 +45,17 @@ def loss_function(W, x, recons_x, h, lam):
     contractive_loss = torch.sum(torch.mm(dh**2, w_sum), 0)
     return mse + contractive_loss.mul_(lam)
 
+
+    
+def contractive_loss(y_pred, y_true):
+    mse = K.mean(K.square(y_true - y_pred), axis=1)
+
+    W = K.variable(value=model.get_layer('encoded').get_weights()[0])  # N x N_hidden
+    W = K.transpose(W)  # N_hidden x N
+    h = model.get_layer('encoded').output
+    dh = h * (1 - h)  # N_batch x N_hidden
+
+    # N_batch x N_hidden * N_hidden x 1 = N_batch x 1
+    contractive = lam * K.sum(dh**2 * K.sum(W**2, axis=1), axis=1)
+
+    return mse + contractive
